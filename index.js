868d2aac618a04b330a96798d01bfd734a1b116c
travis_gandy <travis_gandy@yahoo.com>
2021-04-13 11:57:58 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function saturdayFun(target="roller-skate") {
+    return `This Saturday, I want to ${target}!`;
+}
+
+function mondayWork(target="go to the office") {
+    return `This Monday, I will ${target}.`;
+}
+
+function wrapAdjective(greeting="*") {
+    return function(msg="special") {
+      return `You are ${greeting}${msg}${greeting}!`
+    }
+}
+  
